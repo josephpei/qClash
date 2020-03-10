c55f7542cc74ff2b55f9783c57fe8c2885c65a53
josephpei <josephpei@gmail.com>
2020-03-11 00:48:15 +0800

    program skeleton

---
+++ lang/en_US.ts
@@ -0,0 +1,22 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.1">
+<context>
+    <name>MainWindow</name>
+    <message>
+        <location filename="../src/mainwindow.ui" line="14"/>
+        <source>MainWindow</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="43"/>
+        <source>&amp;Quit</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../src/mainwindow.cpp" line="46"/>
+        <source>Global Mode</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
