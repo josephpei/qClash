812c6060aca4d5bef32de03af6af6aabb4c02e4e
josephpei <josephpei@gmail.com>
2020-03-17 21:15:37 +0800

    make application singleton

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,17 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <singleapplication.h>
 #include <QTranslator>
 
 int main(int argc, char *argv[])
 {
-    QApplication a(argc, argv);
+    SingleApplication a(argc, argv);
     QTranslator translator;
     translator.load(QLocale::system().name(), "./lang");
     a.installTranslator(&translator);
     MainWindow w;
     w.show();
+    QObject::connect(&a, &SingleApplication::instanceStarted, &w, &QMainWindow::raise);
     return a.exec();
 }
